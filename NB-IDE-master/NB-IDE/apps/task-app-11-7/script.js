ca16732e0649393e84a2d8b6959e274260c9deb2
yuma1004 <55342127+yuma1004@users.noreply.github.com>
2019-11-07 20:08:28 +0900

    Add files via upload

--- NB-IDE-master/NB-IDE/apps/task-app-11-7/script.js
+++ NB-IDE-master/NB-IDE/apps/task-app-11-7/script.js
@@ -1,13 +1,25 @@
 // accordion機能
 // header
     $('.btn-header-a').click(function() {
-        $('.header-b').slideToggle(500);
+        $('.header-b').slideToggle(400);
         // $('.header-b').fadeToggle(3000);
     });
     $('.btn-header-b').click(function() {
-        $('.header-a').slideToggle(500);
+        $('.header-a').slideToggle(400);
         // $('.header-b').fadeToggle(3000);
     });
+
+    // アコーディオン機能
+//.accordionがクリックされたら
+$(document).on('click', '.accordion', function() {
+ 
+    console.log($(this).next().slideToggle(400))
+   
+    $(this).toggleClass('open');
+});
+
+
+
 
 // task-app
 // accordion
@@ -35,22 +47,25 @@
 
 
 
-
-
     
 // layer-1-a
 $('.layer-a').sortable();
+$('.layer-b').sortable();
+
+
 
 $(document).on("click",".add-btn-layer-1", function () {
     var html = `
         <div class="layer-1-a">
             <input type="text" class="text-1" placeholder="layer-1=title">
-            <input class="add-btn-layer-2" type="button" value="add-btn-layer-2" >
+            <input class="add-btn-layer-2" type="button" value="add" >
             <button class="remove1">-</button>
             <p class="accordion article-title"></p>
             <ul>
 
             </ul>
+
+            
         </div>
         <script>$('.layer-1-a').sortable();</script>
         <script>$('.layer-1-a ul').sortable();</script>
@@ -74,12 +89,13 @@ $(document).on("click",".add-btn-layer-1", function () {
 // layer-1-b
     var html = `
         <div class="layer-1-b">
-        <p class="accordion article-title"></p>
+        
             <!-- (ココに layer-2-b が出力されます) -->
         <ul class="layer-2-B">
 
         </ul>
           
+        
     `;
     $('.layer-b').append(html);
     // $(this).parents('.task-container').children(".layer-b").append(html);
@@ -105,10 +121,10 @@ $(document).on("click",".add-btn-layer-1", function () {
 $(document).on("click",".add-btn-layer-2", function () {
     var html = `
         <div class="layer-2-a">
-        layer-2-a
+        
             <input class="check-1" type="checkbox">
             <input type="text" class="text-2" placeholder="layer-2=mission-1">
-            <input class="add-btn-layer-3" type="button" value="add-btn-layer-3" >
+            <input class="add-btn-layer-3" type="button" value="add" >
             <button class="remove2">-</button>
             <p class="accordion article-title"></p>
             <ul>
@@ -118,6 +134,7 @@ $(document).on("click",".add-btn-layer-2", function () {
             </ul>
         </div>
         <script>
+        
             $('.layer-2 ul').sortable();
         </script>
     `;
@@ -145,7 +162,7 @@ $(document).on("click",".add-btn-layer-2", function () {
 // layer-2-b
     var html = `
         <div class="layer-2-b">
-        layer-2-b
+       
         
         <h2>
             <!-- 入力連動(ココに出力されます) -->
@@ -154,6 +171,7 @@ $(document).on("click",".add-btn-layer-2", function () {
         <ul class="layer-3-B">
 
         </ul>
+        <script>$('.layer-3-B').sortable();</script>
     `;
 
     // --------------------超重要----------------------------------
@@ -172,7 +190,7 @@ $(document).on("click",".add-btn-layer-2", function () {
     var eclassName = "e"+aclassName.slice(1)
     // console.log("e"+aclassName.slice(1))
 
-    console.log(eclassName)
+    
     $(`.${eclassName}`).children(".layer-2-B").append(html);
     // ------------------------------------------------------
     $('.layer-b').css('padding','10px 10px');
@@ -194,24 +212,17 @@ $(document).on("click",".add-btn-layer-2", function () {
 });
 
 
-
-
-
-
-
-
-
 // layer-3-a
 $(document).on("click",".add-btn-layer-3", function () {
     var html = `
         <div class="layer-3-a">
-        layer-3-a
+     
             <input class="check-1" type="checkbox">
             
             <!-- 入力連動 -->
             <input type="text" class="text text-3" placeholder="layer-3=mission-2">
             
-            <input class="add-btn-layer-4" type="button" value="add-btn-layer-4" >
+            <input class="add-btn-layer-4" type="button" value="add" >
             <button class="remove3">-</button>
             <p class="accordion article-title"></p>
             <ul>
@@ -220,10 +231,10 @@ $(document).on("click",".add-btn-layer-3", function () {
         </div>
 
     <!-- ソート機能 -->
-        <script>      
-            $('.layer-3 ul').sortable({
-                placeholder: "ui-state-highlight"
-            });
+        <script>
+    
+            $('.layer-2-a').sortable(); 
+            
 
         </script>
     `;
@@ -246,11 +257,43 @@ $(document).on("click",".add-btn-layer-3", function () {
     });
 
 
+    // 入力連動
+    $(".layer-3-a .text-3").each(function(){
+    
+        $(this).bind('keyup', hoge(this));
+        
+    });
+        
+    function hoge(elm){
+        var v, old = elm.value;
+        return function(){
+            if(old != (v=elm.value)){
+                old = v;
+                str = $(this).val();
+                
+                console.log($(this).parent().attr('class'))
+                var classVal = $(this).parent().attr('class')
+                var aclassName = classVal.split(' ').slice(-3)[0]
+                var bclassName = classVal.split(' ').slice(-2)[0]
+                var cclassName = classVal.split(' ').slice(-1)[0]
+
+                var eclassName = "e"+aclassName.slice(1)
+                var fclassName = "f"+bclassName.slice(1)
+                var gclassName = "g"+cclassName.slice(1)
+
+                $(`.${eclassName} .${fclassName} .${gclassName}`).find('h2').text(str);
+
+            }
+        }
+    }
+
+
+
 
 //layer-3-b
     var html = `
         <div class="layer-3-b">
-        layer-3-b
+        
         <h2>
             <!-- 入力連動(ココに出力されます) -->
         </h2>
@@ -310,6 +353,9 @@ $(document).on("click",".add-btn-layer-3", function () {
         i++;
 
     });
+
+
+    
   
 });
 
@@ -322,7 +368,7 @@ $(document).on("click",".add-btn-layer-3", function () {
 $(document).on("click",".add-btn-layer-4", function () {
     var html = `
         <div class="layer-4-a">
-        layer-4-a
+       
             <input class="check" type="checkbox" name="check">
 
             <!-- 入力連動 -->
@@ -332,11 +378,13 @@ $(document).on("click",".add-btn-layer-4", function () {
                 <td><button class="remove4">-</button></td>
             </tr>
         </div>
+        <script>
+            $('.layer-3-a ul').sortable(); 
+        </script>
+        
     `;
     $(this).parent('.layer-3-a').children("ul").append(html);
 
-    
-    
 
     // 動的にclassをつける
     // layer-3のクラスを子layer-4に付与する
@@ -353,17 +401,59 @@ $(document).on("click",".add-btn-layer-4", function () {
 
     });
 
+    // 入力連動
+    $(".layer-4-a .text-4").each(function(){
+    
+        $(this).bind('keyup', hoge(this));
+        
+    });
+        
+    function hoge(elm){
+        var v, old = elm.value;
+        return function(){
+            if(old != (v=elm.value)){
+                old = v;
+                str = $(this).val();
+                
+                console.log($(this).parent().attr('class'))
+                var classVal = $(this).parent().attr('class')
+                var aclassName = classVal.split(' ').slice(-4)[0]
+                var bclassName = classVal.split(' ').slice(-3)[0]
+                var cclassName = classVal.split(' ').slice(-2)[0]
+                var dclassName = classVal.split(' ').slice(-1)[0]
+                
+                console.log(classVal.split(' ').slice(-4)[0])
+                console.log(classVal.split(' ').slice(-3)[0])
+                console.log(classVal.split(' ').slice(-2)[0])
+                console.log(classVal.split(' ').slice(-1)[0])
+
+
+                var eclassName = "e"+aclassName.slice(1)
+                var fclassName = "f"+bclassName.slice(1)
+                var gclassName = "g"+cclassName.slice(1)
+                var hclassName = "h"+dclassName.slice(1)
+
+                
+
+
+                $(`.${eclassName} .${fclassName} .${gclassName} .${hclassName}`).find('h3').text(str);
+
+            }
+        }
+    }
+
+
+
 
 
 //layer-4-b
     var html = `
     <div class="layer-4-b">
-    layer-4-b
+ 
         <h3>
             <!-- 入力連動(ココに出力されます) -->
         </h3>
         <input class="add-btn-3" type="button" value="add-btn-3" >
-        <button class="remove4-b">-</button>
         <p class="accordion article-title"></p>
         <ul class="layer-5-B">
 
@@ -476,50 +566,38 @@ $(document).on("click",".add-btn-4", function () {
 
 
 
-// remove機能
+// remove機能連動
 // remove1
 $(document).on('click', '.remove1', function() {
+    
+    // console.log($(this).parent().parent().parent().find('.layer-1-b'))
+    
+    // x = $(this).parent().parent().parent().find('.layer-b')
 
+    
     p = $(this).parent().parent()
-    console.log($(this).parent().parent())
-    $(this).parent('.layer-1-a').remove();
-    resetCls(p)
+    x = $(this).parent().parent().parent().find(".layer-b")
 
-    function resetCls(object) {
-        // aを取得
-        console.log(object.attr('class'))
-        var classVal = object.attr('class')
-
-        var aclassName = classVal.split(' ').slice(0)[0][1]
-        console.log(aclassName)
-
-        i = 1
-
-        object.find('div').each(function () {
-            console.log($(this).attr('class'))
-            
-            $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
-
-            console.log($(this).attr('class'))
-            console.log((aclassName + i))
     
-            $(this).addClass(aclassName + i)
-            console.log($(this).attr('class'))
 
-            i++
-        })
-    }
-    
-    // ココから
+    //  layer-1-b e+iをremove
     console.log($(this).parent().attr('class'))
-    p = $(this).parents().find(".layer-b")
+    var aclassName = $(this).parent().attr('class').split(' ').slice(-1)[0]
+    console.log(aclassName)
+
+    var eclassName = "e"+aclassName.slice(1)
+    console.log(eclassName)
     
-    console.log($(this).parents(".task-container").find(".layer-b").attr("class"))
+    $(this).parent().parent().parent().find(`.${eclassName}`).remove();
+    
+    // layer-1-a a+iをremove
+    $(this).parent('.layer-1-a').remove();
 
-    $(this).parents().find(".layer-b").remove();
-    resetCls(p)
 
-    function resetCls(object) {
+    
+    // aresetClass
+    aresetClass(p)
+    function aresetClass(object) {
         // aを取得
         console.log(object.attr('class'))
         var classVal = object.attr('class')
@@ -528,8 +606,8 @@ $(document).on('click', '.remove1', function() {
         console.log(aclassName)
 
         i = 1
-
-        object.find('div').each(function () {
+        
+        object.find('.layer-1-a').each(function () {
             console.log($(this).attr('class'))
             
             $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
@@ -541,47 +619,218 @@ $(document).on('click', '.remove1', function() {
             console.log($(this).attr('class'))
 
             i++
+            
         })
-    
     }
+
+    // eresetClass
+    eresetClass(x)
+    function eresetClass(object) {
+
+        i = 1
+        
+        console.log(object.parent().find('.layer-1-b'))
+        object.parent().find('.layer-1-b').each(function () {
+            
+            $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
+
+            var eclassName = "e"
+            $(this).addClass(eclassName + i)
+
+            i++
+
+        })
+        
+    }
+    
 
 });
+
+
 
 // remove2
 $(document).on('click', '.remove2', function() {
 
     p = $(this).parent().parent()
-    $(this).parent('.layer-2-a').remove();
-    resetCls(p)
+    console.log($(this).parents(".task-container").find(".layer-2-B").attr('class'))
+    x = $(this).parents(".task-container").find(".layer-2-B")
+    
+    
+    //  layer-2-b e+i f+iをremove
+    console.log($(this).parent().attr('class'))
+    var aclassName = $(this).parent().attr('class').split(' ').slice(-2)[0]
+    // console.log(aclassName)
+    var bclassName = $(this).parent().attr('class').split(' ').slice(-1)[0]
+    // console.log(bclassName)
 
-  
+    // 動的e+i 取得
+    var eclassName = "e"+aclassName.slice(1)
+    // console.log(eclassName)
+
+    // 動的f+i 取得
+    var fclassName = "f"+bclassName.slice(1)
+    // console.log(fclassName)
+
+    console.log($(this).parents(".task-container").find(`.${eclassName} .${fclassName}`).attr('class'))
+    $(this).parents(".task-container").find(`.${eclassName} .${fclassName}`).remove();
+    
+    // layer-2-a をremove
+    $(this).parent('.layer-2-a').remove();
+
+    resetClass(p)
+
+    
+    // fresetClass
+    fresetClass(x)
+    
+    function fresetClass(object) {
+        
+        i = 1
+        
+        object.parent().find(`.${eclassName}`).each(function () {
+            
+            $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
+
+            var fclassName = "f"
+            $(this).addClass(fclassName + i)
+
+            i++
+
+        })
+    }
+
 });
+
+
 // remove3
 $(document).on('click', '.remove3', function() {
 
     p = $(this).parent().parent()
-    $(this).parent('.layer-3-a').remove();
-    resetCls(p)
+
+    x = $(this).parents(".task-container").find(".layer-3-B")
     
+    //  layer-3-b e+i f+i g+iをremove
+
+    console.log($(this).parent().attr('class'))
+    var aclassName = $(this).parent().attr('class').split(' ').slice(-3)[0]
+    // console.log(aclassName)
+    var bclassName = $(this).parent().attr('class').split(' ').slice(-2)[0]
+    // console.log(bclassName)
+    var cclassName = $(this).parent().attr('class').split(' ').slice(-1)[0]
+    // console.log(cclassName)
+
+
+    // 動的e+i 取得
+    var eclassName = "e"+aclassName.slice(1)
+    // console.log(eclassName)
+
+    // 動的f+i 取得
+    var fclassName = "f"+bclassName.slice(1)
+    // console.log(fclassName)
+
+    // 動的g+i 取得
+    var gclassName = "g"+cclassName.slice(1)
+    // console.log(gclassName)
+
+
+    console.log($(this).parents(".task-container").find(`.${eclassName} .${fclassName} .${gclassName}`).attr('class'))
+    $(this).parents(".task-container").find(`.${eclassName} .${fclassName} .${gclassName}`).remove();
+    
+
+    // layer-3-a をremove
+    $(this).parent('.layer-3-a').remove();
+
+    resetClass(p)
+
+    // gresetClass
+    gresetClass(x)
+    
+    function gresetClass(object) {
+        
+        i = 1
+        
+        object.parent().find(`.${fclassName}`).each(function () {
+            
+            $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
+
+            var gclassName = "g"
+            $(this).addClass(gclassName + i)
+
+            i++
+
+        })
+    }
+
 });
+
+
 // remove4
 $(document).on('click', '.remove4', function() {
-    // console.log($(this).parent().attr('class').split(' '))
-    
-    // var classname = ($(this).parent().attr('class').split(' '))
-    // var last = classname.slice(-1)[0]
-    // $(`.${last}`).remove();
 
     p = $(this).parent().parent()
+
+    x = $(this).parents(".task-container").find(".layer-4-B")
+    // console.log(x)
+    
+    //  layer-3-b e+i f+i g+i h+iをremove
+
+    // console.log($(this).parent().attr('class'))
+    var aclassName = $(this).parent().attr('class').split(' ').slice(-4)[0]
+    // console.log(aclassName)
+    var bclassName = $(this).parent().attr('class').split(' ').slice(-3)[0]
+    // console.log(bclassName)
+    var cclassName = $(this).parent().attr('class').split(' ').slice(-2)[0]
+    // console.log(cclassName)
+    var dclassName = $(this).parent().attr('class').split(' ').slice(-1)[0]
+    // console.log(dclassName)
+
+
+    // 動的e+i 取得
+    var eclassName = "e"+aclassName.slice(1)
+    // console.log(eclassName)
+
+    // 動的f+i 取得
+    var fclassName = "f"+bclassName.slice(1)
+    // console.log(fclassName)
+
+    // 動的g+i 取得
+    var gclassName = "g"+cclassName.slice(1)
+    // console.log(gclassName)
+
+    // 動的h+i 取得
+    var hclassName = "h"+dclassName.slice(1)
+    // console.log(hclassName)
+
+
+    console.log($(this).parents(".task-container").find(`.${eclassName} .${fclassName} .${gclassName} .${hclassName}`).attr('class'))
+    $(this).parents(".task-container").find(`.${eclassName} .${fclassName} .${gclassName} .${hclassName}`).remove();
+
+
+    // layer-4-a をremove
     $(this).parent('.layer-4-a').remove();
-    resetCls(p)
-});
-
-// remove機能
+    resetClass(p)
 
 
-$(document).on('click', '.remove4-b', function() {
-    $(this).parents('.layer-4-b').remove();
+    // hresetClass
+    hresetClass(x)
+    
+    function hresetClass(object) {
+        
+        i = 1
+        
+        object.parent().find(`.${gclassName}`).each(function () {
+            
+            $(this).removeClass($(this).attr('class').split(' ').slice(-1)[0])
+
+            var hclassName = "h"
+            $(this).addClass(hclassName + i)
+
+            i++
+
+        })
+    }
+
+
 });
 
 $(document).on('click', '.remove5', function() {
@@ -595,7 +844,7 @@ $(document).on('click', '.remove6', function() {
 
 
 
-function resetCls(object) {
+function resetClass(object) {
     // cを取得
     console.log(object.parent())
     var classVal = object.parent().attr('class')
